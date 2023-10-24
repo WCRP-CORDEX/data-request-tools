e37f9f264f236f26dc6ddaa61446cea3b11204c5
Lars Buntemeyer <lars.buntemeyer@hereon.de>
2023-10-24 14:36:21 +0200

    update table creation

--- data_request/tests/test_data_request.py
+++ data_request/tests/test_data_request.py
@@ -1,2 +1,12 @@
-def test_data_request():
-    pass
+import pandas as pd
+
+from .. import create_cmor_tables
+
+data_request_table = "https://raw.githubusercontent.com/WCRP-CORDEX/cordex-cmip6-data-request/main/tables/cordex-cmip6-data-request-extended.csv"
+
+
+def test_create_cordex_cmor_table(table):
+    print(f"retrieving data request from {table}")
+    df = pd.read_csv(table).fillna("")
+    create_cmor_tables(df)
+    return
